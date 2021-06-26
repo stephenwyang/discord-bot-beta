86c7db5ec8a2462741d038926359b1ee34f3c975
Stephen <stephen.cr.yang@gmail.com>
2021-06-26 00:44:24 -0700

    messing around in jquery more to extract data

--- src/util/extract-info.js
+++ src/util/extract-info.js
@@ -14,16 +14,20 @@ module.exports = async (url) => {
         console.log(`${charName} - ${charTitle}`);
         console.log(`ID: ${charID}`);
         var charRarity = $("a", ".char-rarity").eq(0).attr('title').split(" ")[0];
-        console.log(charRarity);
+        //console.log(charRarity);
         var charEle = getElement($('tr>td', ".tabbertab[title='Stats']").eq(2).text());
-        console.log(charEle);
+        //console.log(charEle);
+
         //To get ougi/charge attack, table is in $(".table-container").eq(1)
         //Figuring out how to extract those values next
         //In JQuery, $(".wikitable>tbody>tr:gt(1)",".table-container:eq(1)").text()
+        // $(".wikitable>tbody>tr:gt(1)",".table-container:eq(1)").children().each(function (i, elem) { console.log($(this).text()) })
+        // This will get each td and display is, but still has the problem of the children (can prob do the remove stuff from before)
+        // Still can't translate it to cheerio tho
         //Will extract all the values for the "ougi", but is unable to split between lines
         //Figure out how to extract for cheerio
-        //var charOugi = $(".wikitable>tbody>tr",".table-container").eq(1).text()
-        //console.log(charOugi)
+        var charOugi = $(".wikitable>tbody>tr",".table-container").eq(1).text()
+        console.log(charOugi)
     })
     //error handling
     .catch(err => {
