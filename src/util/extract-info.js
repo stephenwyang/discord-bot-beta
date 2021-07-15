ece2f702e8059e654b8b4b2c301366a02ae0e47e
Stephen <stephen.cr.yang@gmail.com>
2021-07-15 01:30:54 -0700

    text cleanup, skill extraction 20%

--- src/util/extract-info.js
+++ src/util/extract-info.js
@@ -31,27 +31,37 @@ module.exports = async (url) => {
                     if ( (i-1) % 3 === 0 ) {
                         //console.log("")
                         ougiIdx += 1;
-                        console.log(ougiIdx)
+                        //console.log(ougiIdx)
                     }
                     // Currently will remove all .tooltiptext values (Extra info), want to have a way to add it in a way thats
                     // less intrusive in the future
                     else if ( (i-2) % 3 === 0 ) {
-                        ougiNames[ougiIdx] = $(this).text()
+                        ougiNames[ougiIdx] = $(this).text().trim()
                         //console.log($(this).text())
-                        console.log(ougiNames[ougiIdx])
+                        //console.log(ougiNames[ougiIdx])
                     }
                     else { 
                         //console.log($(".tooltip>tooltiptext", this).remove().end().text()) 
                         $(".tooltip", this).each( function (i, elem) {
                             $(".tooltiptext", this).remove()
                         })
-                        ougiDescript[ougiIdx] = $(this).text()
+                        $(this).find('br').before(' ');
+                        ougiDescript[ougiIdx] = $(this).text().trim()
                         //console.log($(this).text())
-                        console.log(ougiDescript[ougiIdx])
+                        //console.log(ougiDescript[ougiIdx])
                     }
                 }
             })
         })
+
+        // This is strictly for testing purposes, will be commented out/removed
+        // on full release
+        // Printing out ougi: Names/Descriptions
+
+        for (let i = 0; i < ougiNames.length; i++) {
+            console.log(`${ougiNames[i]} - ${ougiDescript[i]}`)
+        }
+
 
         //console.log(ougiDescript.length)
 
@@ -60,18 +70,36 @@ module.exports = async (url) => {
         //For dual form charas, most of them have a .table-container length of 9
         //Might be able to make a filter for that
 
-        dform_v = $(".table-container").length
-        console.log(dform_v)
+        const testV = $(".table-container").length
+        var twoFormBool
+        if (testV === 8) {
+            twoFormBool = false
+            console.log("This character has one skill set")
+        }
+        else if (testV === 9) {
+            twoFormBool = true
+            console.log("This character has two skill sets")
+        }
 
         $(".table-container:eq(2)").each (function (i, elem) {
             $(".wikitable>tbody>tr", this).children().each( function (i, elem) {
                 if (i === 0 || i > 6) {
                     //Saving skill-set name with i === 0
                     //Maybe have it as a separate branch
+                    $(".tooltip", this).each( function (i, elem) {
+                        $(".tooltiptext", this).remove()
+                    })
                     //console.log(i)
                     //console.log($(this).text())
                 }
-                
+                /*else if ( (i + 1) % 6 === 0 ) {
+                    $(".tooltip", this).each( function (i, elem) {
+                        $(".tooltiptext", this).remove()
+                    })
+                    $(this).find('br').before(' ');
+                    console.log(i)
+                    console.log($(this).text())
+                } */ //Work on this later, good example to clean up some text
             })
         })
 
